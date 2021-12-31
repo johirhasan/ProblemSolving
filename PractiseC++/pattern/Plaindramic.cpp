0e07d51a98a604fa82940898dd973d34def98c7a
Johir <johirhasan10203@gmail.com>
2021-12-31 22:02:09 +0600

    Added a new pattern

---
+++ PractiseC++/pattern/Plaindramic.cpp
@@ -0,0 +1,27 @@
+//palindramic pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, k, m, n;
+    cout<<"Enter a number: ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (k = i; k >= 2; k--)
+        {
+            cout << k << " ";
+        }
+        for (m = 1; m <= i; m++)
+        {
+            cout << m << " ";
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
